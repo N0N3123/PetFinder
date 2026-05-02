ba94f8d117d4294e038ce28194b5b09ac74ba0b0
Bartosz <bartoszbanach199@gmail.com>
2026-05-02 23:36:14 +0200

    Fix PWA manifest

--- vite.config.js
+++ vite.config.js
@@ -18,16 +18,16 @@ export default defineConfig({
         theme_color: '#198754',
         background_color: '#ffffff',
         display: 'standalone',
-        scope: '/',
-        start_url: '/',
+        scope: '/PetFinder/',
+        start_url: '/PetFinder/',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: '/PetFinder/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: '/PetFinder/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
